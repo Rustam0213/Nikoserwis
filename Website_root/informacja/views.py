128c28473df64aeb6faee49df044c5463632c165
Rustam0213 <rdvelihanov1245@gmail.com>
2023-08-06 23:38:06 +0200

    rename_to_index

--- Website_root/informacja/views.py
+++ Website_root/informacja/views.py
@@ -3,7 +3,7 @@ from .models import News
 
 def main_page(request):
     news = News.objects.all()
-    return render(request, './wiadomosci.html',{'news':news})
+    return render(request, './index.html',{'news':news})
 
 def o_nas(request):
     return render(request, './o_nas.html')
